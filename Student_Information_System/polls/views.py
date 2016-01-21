a9c370d54ad3f8dd29a44a7c039600813d3d859e
luukhoai <luukhoai@gmail.com>
2016-01-21 23:49:31 +0700

    Part_4_2_ListView, DetailView
    Part_4_2_ListView, DetailView

--- Student_Information_System/polls/views.py
+++ Student_Information_System/polls/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from polls.models import Question, Choice
 from django.core.urlresolvers import reverse
+from django.views import generic
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -41,4 +42,20 @@ def loadTemplate(request):
     question_list = Question.objects.order_by("-pub_date")[:5]
     context = {'latest_question_list':question_list}
     return render(request, 'polls/loadtemplate.html', context)
+    
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+    
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
+        
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+    
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+    
     
